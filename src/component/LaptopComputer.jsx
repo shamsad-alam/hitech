011668d5a6ffdf1b5eaa0c13b43c516ce4a80133
shamsad-alam <alamshamsad687@gmail.com>
2025-10-15 17:48:21 +0530

    card slider function add

--- src/component/LaptopComputer.jsx
+++ src/component/LaptopComputer.jsx
@@ -1,103 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { FaStar } from "react-icons/fa6";
+
+
 
 function LaptopComputer() {
+    const [cardIndex, setcardIndex] = useState(0)
+    const cardResponsive = window.innerWidth < 640 ? 1 : window.innerWidth < 1024 ? 2 : 3
+
+    const handleNext = () => {
+        if (cardIndex + cardResponsive < dataList.length) {
+            setcardIndex(cardIndex + 1)
+        }
+    }
+    const handlePrev = () => {
+        if (cardIndex > 0) {
+            setcardIndex(cardIndex - 1)
+        }
+
+    }
     return (
         <>
             <div className='w-[90%] mx-auto py-10'>
                 <div>
-                    <div className='grid  lg:grid-cols-5 items-center gap-4 mb-5'>
-                        <h2 className='text-2xl font-semibold' ><span className='text-[#ff3333]'>Laptop</span> & Computer</h2>
-                        <div className='col-span-4 border-b-1 border-zinc-400'></div>
+                    <div className='grid grid-cols-3 md:grid-cols-12 items-center justify-between gap-4 mb-5'>
+                        <h2 className='text-2xl col-span-7 lg:col-span-3 -order-2 font-semibold' ><span className='text-[#ff3333]'>Laptop </span> & Computer</h2>
+                        <div className='col-span-8 border-b-1 border-zinc-400'></div>
+                        <div className='relative flex gap-4 justify-end -order-1 md:order-1'>
+                            <button onClick={handlePrev} className='cursor-pointer'><FaChevronLeft /></button>
+                            <button onClick={handleNext} className='cursor-pointer'><FaChevronRight /></button>
+                        </div>
                     </div>
+
                     <div className='flex flex-col lg:flex-row gap-6'>
-                        <img src="https://dostguru.com/HTML/hitech/assets/img/c&l.jpg" alt="" />
+                        <img src="https://dostguru.com/HTML/hitech/assets/img/c&l.jpg" className='order-3' alt="" />
 
-                        <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6'>
-                            <div className='bg-white flex  gap-4 items-center shadow-lg rounded-lg p-5'>
-                                <div className='w-25'>
-                                    <img src="https://dostguru.com/HTML/hitech/assets/img/product/product_6.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <h3 className='text-md'>Smart Watch Black</h3>
-                                    <span className='my-7'>star here</span>
-                                    <div className='flex gap-2'>
-                                        <span className='text-[#ff3333] font-semibold'>$30.00</span>
-                                        <span className='text-gray-500 line-through'>$40.00</span>
+                        <div className='overflow-hidden order-1'>
+                            <div className='flex transition-all duration-500' style={{ transform: `translateX(-${(cardIndex * 100) / cardResponsive}%)` }}>
+                                {dataList.map((list, index) => (
+                                    <div key={index} className='flex-none w-full md:w-1/2 lg:w-1/3 p-3'>
+                                        <div className='bg-white flex flex-row gap-4 items-center shadow-md rounded-lg p-5'>
+                                            <div className='w-25'>
+                                                <img src={list.image} alt="" />
+                                            </div>
+                                            <div>
+                                                <h3 className='text-lg font-semibold '>{list.title} </h3>
+                                                <span className='mt-4 flex text-sm text-red-600 gap-0.5'><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></span>
+                                                <div className='flex gap-2'>
+                                                    <span className='text-[#ff3333] font-semibold'>${list.mainPrice}</span>
+                                                    <span className='text-gray-500 line-through'>${list.subPrice}</span>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-
-                            <div className='bg-white flex  gap-4 items-center shadow-lg rounded-lg p-5'>
-                                <div className='w-25'>
-                                    <img src="https://dostguru.com/HTML/hitech/assets/img/product/product_6.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <h3 className='text-md'>Smart Watch Black</h3>
-                                    <span className='my-7'>star here</span>
-                                    <div className='flex gap-2'>
-                                        <span className='text-[#ff3333] font-semibold'>$30.00</span>
-                                        <span className='text-gray-500 line-through'>$40.00</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='bg-white flex  gap-4 items-center shadow-lg rounded-lg p-5'>
-                                <div className='w-25'>
-                                    <img src="https://dostguru.com/HTML/hitech/assets/img/product/product_6.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <h3 className='text-md'>Smart Watch Black</h3>
-                                    <span className='my-7'>star here</span>
-                                    <div className='flex gap-2'>
-                                        <span className='text-[#ff3333] font-semibold'>$30.00</span>
-                                        <span className='text-gray-500 line-through'>$40.00</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='bg-white flex  gap-4 items-center shadow-lg rounded-lg p-5'>
-                                <div className='w-25'>
-                                    <img src="https://dostguru.com/HTML/hitech/assets/img/product/product_6.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <h3 className='text-md'>Smart Watch Black</h3>
-                                    <span className='my-7'>star here</span>
-                                    <div className='flex gap-2'>
-                                        <span className='text-[#ff3333] font-semibold'>$30.00</span>
-                                        <span className='text-gray-500 line-through'>$40.00</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='bg-white flex  gap-4 items-center shadow-lg rounded-lg p-5'>
-                                <div className='w-25'>
-                                    <img src="https://dostguru.com/HTML/hitech/assets/img/product/product_6.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <h3 className='text-md'>Smart Watch Black</h3>
-                                    <span className='my-7'>star here</span>
-                                    <div className='flex gap-2'>
-                                        <span className='text-[#ff3333] font-semibold'>$30.00</span>
-                                        <span className='text-gray-500 line-through'>$40.00</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='bg-white flex  gap-4 items-center shadow-lg rounded-lg p-5'>
-                                <div className='w-25'>
-                                    <img src="https://dostguru.com/HTML/hitech/assets/img/product/product_6.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <h3 className='text-md'>Smart Watch Black</h3>
-                                    <span className='my-7'>star here</span>
-                                    <div className='flex gap-2'>
-                                        <span className='text-[#ff3333] font-semibold'>$30.00</span>
-                                        <span className='text-gray-500 line-through'>$40.00</span>
-                                    </div>
-                                </div>
+                                ))}
                             </div>
                         </div>
+
+            
+
                     </div>
                 </div>
             </div>
         </>
     )
 }
+
+const dataList = [
+    { image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_10.jpg', title: 'New Hp Laptop best', mainPrice: '30.00', subPrice: '40.00' },
+    { image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_10.jpg', title: 'New Hp Laptop best', mainPrice: '30.00', subPrice: '40.00' },
+    { image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_10.jpg', title: 'New Hp Laptop best', mainPrice: '30.00', subPrice: '40.00' },
+    { image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_10.jpg', title: 'New Hp Laptop best', mainPrice: '30.00', subPrice: '40.00' },
+    { image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_10.jpg', title: 'New Hp Laptop best', mainPrice: '30.00', subPrice: '40.00' },
+    { image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_10.jpg', title: 'New Hp Laptop best', mainPrice: '30.00', subPrice: '40.00' },
+    { image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_10.jpg', title: 'New Hp Laptop best', mainPrice: '30.00', subPrice: '40.00' },
+
+]
+
 
 export default LaptopComputer
--- src/component/TopProduct.jsx
+++ src/component/TopProduct.jsx
@@ -1,27 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { FaStar } from "react-icons/fa6";
+
+
 
 function TopProduct() {
+    const [cardindex, setcardindex] = useState(0)
+
+    const cardResponsive = window.innerWidth < 640 ? 1 : window.innerWidth < 1024 ? 2 : 5
+
+    const handleLeft = () => {
+        if (cardindex > 0) {
+            setcardindex(cardindex - 1)
+        }
+
+    }
+    const handleRight = () => {
+        if (cardindex + cardResponsive < productList.length) {
+            setcardindex(cardindex + 1)
+        }
+
+    }
 
     return (
         <>
             <div className='w-[90%] mx-auto py-10'>
                 <div>
-                    <div className='grid  lg:grid-cols-7 items-center gap-4 mb-5'>
+                    <div className='grid lg:grid-cols-7 items-center justify-between gap-4 mb-5'>
                         <h2 className='text-2xl font-semibold' ><span className='text-[#ff3333]'>Top</span> Products</h2>
-                        <div className='col-span-6 border-b-1 border-zinc-400'></div>
+                        <div className=' md:col-span-5 border-b-1 border-zinc-400'></div>
+                        <div className='ml-auto flex gap-3 '>
+                            <button onClick={handleLeft} className='cursor-pointer'><FaChevronLeft /></button>
+                            <button onClick={handleRight} className='cursor-pointer'><FaChevronRight /></button>
+                        </div>
                     </div>
 
                     <div className='overflow-hidden'>
-                        <div className='flex'>
+                        <div className='flex transition-all duration-500' style={{ transform: `translateX(-${(cardindex * 100) / cardResponsive}%)` }}>
                             {
                                 productList.map((list, index) => (
                                     <div key={index} className=' flex-none w-full md:w-1/2 lg:w-1/5 p-3'>
-                                        <div className='bg-white flex flex-col items-center shadow-lg rounded-lg p-4'>
+                                        <div className='bg-white flex flex-col items-center shadow-md rounded-lg p-4'>
                                             <div className='border-b-1 pb-3 border-gray-200'>
                                                 <img src={list.image} alt="" />
                                             </div>
-                                            <span className='mt-5'>{list.star} </span>
-                                            <h3 className='text-xl  my-1'>{list.tite} </h3>
+                                            <span className='mt-6 flex text-sm text-red-600 gap-0.5'><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /> </span>
+                                            <h3 className='text-xl font-semibold  my-2'>{list.tite} </h3>
                                             <div className='flex gap-2'>
                                                 <span className='text-[#ff3333] font-semibold'>{list.mainPrice} </span>
                                                 <span className='text-gray-500 line-through'>{list.subPrice} </span>
@@ -33,91 +57,22 @@ function TopProduct() {
                         </div>
                     </div>
 
-                    {/* <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-6 min-h-94 gap-6'>
-                        <div className='bg-white flex flex-col items-center shadow-lg rounded-lg p-4'>
-                            <div className='border-b-1 pb-3 border-gray-200'>
-                                <img src="https://dostguru.com/HTML/hitech/assets/img/product/deal_1.jpg" alt="" />
-                            </div>
-                            <span className='mt-5'>star here</span>
-                            <h3 className='text-xl  my-1'>Smart Watch Black</h3>
-                            <div className='flex gap-2'>
-                                <span className='text-[#ff3333] font-semibold'>$30.00</span>
-                                <span className='text-gray-500 line-through'>$40.00</span>
-                            </div>
-                        </div>
-                        <div className='bg-white flex flex-col items-center shadow-lg rounded-lg p-4'>
-                            <div className='border-b-1 pb-3 border-gray-200'>
-                                <img src="https://dostguru.com/HTML/hitech/assets/img/product/deal_1.jpg" alt="" />
-                            </div>
-                            <span className='mt-5'>star here</span>
-                            <h3 className='text-xl  my-1'>Smart Watch Black</h3>
-                            <div className='flex gap-2'>
-                                <span className='text-[#ff3333] font-semibold'>$30.00</span>
-                                <span className='text-gray-500 line-through'>$40.00</span>
-                            </div>
-                        </div>
-                        <div className='bg-white flex flex-col items-center shadow-lg rounded-lg p-4'>
-                            <div className='border-b-1 pb-3 border-gray-200'>
-                                <img src="https://dostguru.com/HTML/hitech/assets/img/product/deal_1.jpg" alt="" />
-                            </div>
-                            <span className='mt-5'>star here</span>
-                            <h3 className='text-xl  my-1'>Smart Watch Black</h3>
-                            <div className='flex gap-2'>
-                                <span className='text-[#ff3333] font-semibold'>$30.00</span>
-                                <span className='text-gray-500 line-through'>$40.00</span>
-                            </div>
-                        </div>
-                        <div className='bg-white flex flex-col items-center shadow-lg rounded-lg p-4'>
-                            <div className='border-b-1 pb-3 border-gray-200'>
-                                <img src="https://dostguru.com/HTML/hitech/assets/img/product/deal_1.jpg" alt="" />
-                            </div>
-                            <span className='mt-5'>star here</span>
-                            <h3 className='text-xl  my-1'>Smart Watch Black</h3>
-                            <div className='flex gap-2'>
-                                <span className='text-[#ff3333] font-semibold'>$30.00</span>
-                                <span className='text-gray-500 line-through'>$40.00</span>
-                            </div>
-                        </div>
-                        <div className='bg-white flex flex-col items-center shadow-lg rounded-lg p-4'>
-                            <div className='border-b-1 pb-3 border-gray-200'>
-                                <img src="https://dostguru.com/HTML/hitech/assets/img/product/deal_1.jpg" alt="" />
-                            </div>
-                            <span className='mt-5'>star here</span>
-                            <h3 className='text-xl  my-1'>Smart Watch Black</h3>
-                            <div className='flex gap-2'>
-                                <span className='text-[#ff3333] font-semibold'>$30.00</span>
-                                <span className='text-gray-500 line-through'>$40.00</span>
-                            </div>
-
-                        </div>
-                        <div className='bg-white flex flex-col items-center shadow-lg rounded-lg p-4'>
-                            <div className='border-b-1 pb-3 border-gray-200'>
-                                <img src="https://dostguru.com/HTML/hitech/assets/img/product/deal_1.jpg" alt="" />
-                            </div>
-                            <span className='mt-5'>star here</span>
-                            <h3 className='text-xl  my-1'>Smart Watch Black</h3>
-                            <div className='flex gap-2'>
-                                <span className='text-[#ff3333] font-semibold'>$30.00</span>
-                                <span className='text-gray-500 line-through'>$40.00</span>
-                            </div>
-                        </div>
-                    </div> */}
-
                 </div>
             </div>
+
             <SpecialTwoCard />
         </>
     )
 }
 
 const productList = [
+    { id: 1, image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_22.jpg', star: <FaStar />, tite: 'Smart Watch black', mainPrice: '30.00', subPrice: '40.00' },
     { id: 1, image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_14.jpg', star: 'start here', tite: 'Smart Watch black', mainPrice: '30.00', subPrice: '40.00' },
+    { id: 1, image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_18.jpg', star: 'start here', tite: 'Smart Watch black', mainPrice: '30.00', subPrice: '40.00' },
     { id: 1, image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_14.jpg', star: 'start here', tite: 'Smart Watch black', mainPrice: '30.00', subPrice: '40.00' },
+    { id: 1, image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_20.jpg', star: 'start here', tite: 'Smart Watch black', mainPrice: '30.00', subPrice: '40.00' },
     { id: 1, image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_14.jpg', star: 'start here', tite: 'Smart Watch black', mainPrice: '30.00', subPrice: '40.00' },
-    { id: 1, image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_14.jpg', star: 'start here', tite: 'Smart Watch black', mainPrice: '30.00', subPrice: '40.00' },
-    { id: 1, image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_14.jpg', star: 'start here', tite: 'Smart Watch black', mainPrice: '30.00', subPrice: '40.00' },
-    { id: 1, image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_14.jpg', star: 'start here', tite: 'Smart Watch black', mainPrice: '30.00', subPrice: '40.00' },
-    { id: 1, image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_14.jpg', star: 'start here', tite: 'Smart Watch black', mainPrice: '30.00', subPrice: '40.00' },
+    { id: 1, image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_22.jpg', star: 'start here', tite: 'Smart Watch black', mainPrice: '30.00', subPrice: '40.00' },
     { id: 1, image: 'https://dostguru.com/HTML/hitech/assets/img/product/product_14.jpg', star: 'start here', tite: 'Smart Watch black', mainPrice: '30.00', subPrice: '40.00' },
 ]
 
