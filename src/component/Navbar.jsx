b8e92889ab4d150bf3f618408aefac622f709d74
shamsad-alam <alamshamsad687@gmail.com>
2025-10-16 14:27:00 +0530

    navbar toggle style

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaCartArrowDown } from "react-icons/fa6";
 import { IoPersonCircle } from "react-icons/io5";
 import { HiMenuAlt2 } from "react-icons/hi";
@@ -9,6 +9,12 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 
 
 function Navbar() {
+    const [isOpen, setisOpen] = useState(false)
+
+    const handleOpen = () => {
+        setisOpen(!isOpen)
+    }
+
     return (
         <>
             <div className='w-full bg-white shadow-md sticky top-0 z-50'>
@@ -28,7 +34,7 @@ function Navbar() {
                 <div className='border-b-1 border-gray-300'>
                     <div className='max-w-[90%] mx-auto flex justify-between items-center md:py-6 py-4'>
                         <div className='flex gap-2 items-center '>
-                            <span className='text-[25px] text-red-600 cursor-pointer md:hidden'><HiMenuAlt2 /></span>
+                            <span onClick={handleOpen} className='text-[25px] text-red-600 cursor-pointer md:hidden '><HiMenuAlt2 /></span>
                             <h1 className='text-3xl font-bold'>Hi<span className='text-red-600'>T</span>ech</h1>
                         </div>
 
@@ -50,7 +56,8 @@ function Navbar() {
                             <div className='md:flex hidden'><h3>Favourate</h3></div>
                             <div className='flex gap-2 items-center'>
                                 <span className=' text-[25px] cursor-pointer md:hidden'><IoPersonCircle /></span>
-                                <span className='flex text-2xl cursor-pointer'><FaCartArrowDown /></span>
+                                <span className='flex text-2xl cursor-pointer'><FaCartArrowDown />
+                                </span>
                             </div>
                         </div>
                     </div>
@@ -88,34 +95,40 @@ function Navbar() {
                 </div>
 
                 {/* ==================== Mobile version nabvar ================= */}
-                <div className='py-4 shadow-none md:shadow-lg flex md:hidden'>
-                    <div className='md:flex max-w-[90%] mx-auto justify-between items-center hidden'>
-                        <div className='w-full flex flex-col z-99 absolute top-[70px] left-0 px-6 py-4 bg-white shadow-md gap-4 font-semibold cursor-pointer'>
-                            <div><h3>Home</h3></div>
-                            <div><h3>Shop</h3></div>
-                            <div><h3>Blog</h3></div>
-                            <div><h3>Gallary</h3></div>
-                            <div><h3>Products</h3></div>
-                            <div><h3>About Us</h3></div>
-                            <div><h3>Contact Us</h3></div>
-                            <div className='relative group'>
-                                <div>
-                                    <div className='flex gap-1 items-center'>
-                                        <h3>Pages </h3>
-                                        <span className='text-2xl'><RiArrowDropDownLine /></span>
-                                    </div>
-                                    <div className='absolute w-[200px] left-0 top-full bg-white text-black p-4 hidden group-hover:flex flex-col gap-2 shadow-lg'>
-                                        <div className='py-1'>Action</div>
-                                        <div className='py-1'>Another Action</div>
-                                        <div className='py-1'>Something Else</div>
+                <div>
+                    {
+                        isOpen && (
+                            <div className=''>
+                                <div className={`w-full flex flex-col z-99 absolute top-[70px]  px-6 py-4
+                                     bg-white shadow-md gap-4 font-semibold cursor-pointer overflow-hidden transition-all duration-500 ease-in-out
+                                      ${isOpen ? "left-0" : "100%"}`}>
+                                    <div><h3>Home</h3></div>
+                                    <div><h3>Shop</h3></div>
+                                    <div><h3>Blog</h3></div>
+                                    <div><h3>Gallary</h3></div>
+                                    <div><h3>Products</h3></div>
+                                    <div><h3>About Us</h3></div>
+                                    <div><h3>Contact Us</h3></div>
+                                    <div className='relative group'>
+                                        <div>
+                                            <div className='flex gap-1 items-center'>
+                                                <h3>Pages </h3>
+                                                <span className='text-2xl'><RiArrowDropDownLine /></span>
+                                            </div>
+                                            <div className='absolute w-[200px] left-0 top-full bg-white text-black p-4 hidden group-hover:flex flex-col gap-2 shadow-lg'>
+                                                <div className='py-1'>Action</div>
+                                                <div className='py-1'>Another Action</div>
+                                                <div className='py-1'>Something Else</div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
+                                <div>
+                                    <h3>Hotline: +880 123 456 789</h3>
+                                </div>
                             </div>
-                        </div>
-                        <div>
-                            <h3>Hotline: +880 123 456 789</h3>
-                        </div>
-                    </div>
+                        )
+                    }
                 </div>
             </div>
         </>
