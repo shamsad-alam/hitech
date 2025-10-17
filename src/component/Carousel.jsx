1bf26b64d87c7e75840f73df74b4f1061acd53c4
shamsad-alam <alamshamsad687@gmail.com>
2025-10-17 11:30:49 +0530

    create banner component

---
+++ src/component/Carousel.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Carousel() {
+    return (
+        <>
+            <section>
+                <div className='w-[90%] mx-auto py-10 '>
+                    <div>
+                        {/* Aside menu (categeory) */}
+                        <div></div>
+
+                        {/* Carousel banner */}
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Carousel
