1bf01335602ea9500c6c7b5710a6bdfe44851ccb
shamsad-alam <alamshamsad687@gmail.com>
2025-10-25 14:01:19 +0530

    some change

--- src/practice/day1/PracticeDay1.jsx
+++ src/practice/day1/PracticeDay1.jsx
@@ -7,6 +7,34 @@ function PracticeDay1() {
     const [Inputval, setInputval] = useState()
 
     const stdName = ["shamsad", "syam", "rahul", "mark", "sikandar"]
+
+    const [stdData, setstdData] = useState({
+        name: 'shamsad',
+        age: 35,
+        address: {
+            city: "kolkata",
+            country: 'india'
+        }
+    })
+
+    const handleAge = (ag) => {
+        stdData.age = ag
+        console.log(stdData);
+        setstdData({ ...stdData })
+    }
+
+    const handlecity = (c) => {
+        stdData.address.city = c
+        console.log(stdData);
+
+        setstdData({ ...stdData, address: { ...stdData.address, c } })
+    }
+
+    const updatcontry = () => {
+        setstdData({
+            ...stdData, address: { ...stdData.address, country: 'japan' }
+        })
+    }
     return (
         <div className='w-7xl mt-10 mx-auto'>
             <h3>This is my first react.js task.</h3>
@@ -35,8 +63,22 @@ function PracticeDay1() {
             </div>
 
             <div className='my-5 border-1 p-5'>
-                <input type="text" value={Inputval} placeholder='enter text here...' className='border-1 py-1 px-3' />
-                <p> Input value: {setInputval} </p>
+                <input type="text" onChange={(e) => setInputval(e.target.value)} placeholder='enter text here...' className='border-1 py-1 px-3' />
+                <p> Input value: {Inputval} </p>
+            </div>
+
+            <div className='my-5 p-5 border-1'>
+                <h3 className=' text-xl font-semibold my-2'>object and array dat change by state</h3>
+                <input className='border-1' type="number" onChange={(e) => handleAge(e.target.value)} placeholder='enter age' />
+                <p> Age : {stdData.age}</p>
+
+                <input type="text" onChange={(e) => handlecity(e.target.value)} placeholder='enter city' className='border-1' />
+                <p>City : {stdData.address.city} </p>
+
+                <button onClick={updatcontry} className='border-1 bg-zinc-100 rounded-md py-1 px-3'>update country</button>
+                <p>Country : {stdData.address.country} </p>
+
+
             </div>
 
         </div>
