acf82d02121a66ef109294d3ce4b021b8814a7df
shamsad-alam <alamshamsad687@gmail.com>
2025-10-28 15:03:07 +0530

    practice day 1 compoentent some changes

--- src/practice/day1/PracticeDay1.jsx
+++ src/practice/day1/PracticeDay1.jsx
@@ -35,6 +35,16 @@ function PracticeDay1() {
             ...stdData, address: { ...stdData.address, country: 'japan' }
         })
     }
+
+    const [employ, setemploy] = useState(["shamsad", "sikandaer", "vishal", "pitter", "jamal"])
+    const [empInput, setempInput] = useState('')
+
+    const emphandlechnge = () => {
+        // let sham = employ[4] = e
+        // let empdata = empInput
+        setemploy([...employ, empInput ])
+        setempInput('')
+    }
     return (
         <div className='w-7xl mt-10 mx-auto'>
             <h3>This is my first react.js task.</h3>
@@ -62,11 +72,13 @@ function PracticeDay1() {
                 </button>
             </div>
 
+            {/* add input data and show on user page. */}
             <div className='my-5 border-1 p-5'>
                 <input type="text" onChange={(e) => setInputval(e.target.value)} placeholder='enter text here...' className='border-1 py-1 px-3' />
                 <p> Input value: {Inputval} </p>
             </div>
 
+            {/* object change by state, using input and button */}
             <div className='my-5 p-5 border-1'>
                 <h3 className=' text-xl font-semibold my-2'>object and array dat change by state</h3>
                 <input className='border-1' type="number" onChange={(e) => handleAge(e.target.value)} placeholder='enter age' />
@@ -79,6 +91,19 @@ function PracticeDay1() {
                 <p>Country : {stdData.address.country} </p>
 
 
+            </div>
+
+            {/* array change by state, using input and button */}
+            <div className='border-1 p-5 my-5'>
+                <input type="text" value={empInput} onChange={(e) => setempInput(e.target.value)} placeholder='enter name' className='border-1 p-2' />
+                <button onClick={emphandlechnge} className='p-2 bg-amber-100 border-1 cursor-pointer'> Add New User</button>
+                {
+                    employ.map((emp) => (
+                        <ul key={emp}>
+                            <li>{emp} </li>
+                        </ul>
+                    ))
+                }
             </div>
 
         </div>
